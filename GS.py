d0cd93fb5ada61027aa4cc3f9d2c1ec59a593ca2
Yongli Chen <saiyan@wirelessprvnat-172-17-83-248.near.illinois.edu>
2015-11-18 20:42:29 -0600

    I think GS.py is almost done, but still need to debug.

---
+++ GS.py
@@ -0,0 +1,57 @@
+import numpy as np
+from scipy.linalg import solve
+
+def gauss(Q, zero, pi, n, dim, lamda):
+    pi_next = [0.0, 0.0, 0.0, 0.0]
+    count = 0
+    #print "Q=",Q,"zero=",zero,"pi=",pi,"dim=",dim,"lamda=",lamda
+    for k in range(1,n):
+        for i in range(1,dim+1):
+            count = 0
+            for j in range(1,i):
+                #print "i=",i,"j=",j
+                pi_next[i-1] += (-1.0)*(pi_next[j-1]*Q[j-1][i-1])/Q[i-1][i-1]
+                '''
+                print "i=",i,"j=",j,"upper_half"
+                print "Pi[",i,"]",pi[i-1]
+                print "Q[",j,"]","[",i,"]=",Q[j-1][i-1]
+                print "Q[",i,"]","[",i,"]=",Q[i-1][i-1]
+                print "pi_next[",i-1,"]",pi_next[i-1]
+                '''
+            for j in range(i+1,dim+1):
+                #print "i=",i,"j=",j
+                pi_next[i-1] += (-1.0)*(pi[j-1]*Q[j-1][i-1])/Q[i-1][i-1]
+                '''
+                print "i=",i,"j=",j,"lower_half"
+                print "Pi[",i,"]",pi[i-1]
+                print "Q[",j,"]","[",i,"]=",Q[j-1][i-1]
+                print "Q[",i,"]","[",i,"]=",Q[i-1][i-1]
+                print "pi_next[",i-1,"]",pi_next[i-1]
+            print str(k).zfill(4),
+            print(pi_next)
+                '''
+            if(pi_next[i-1]-pi[i-1]<lamda):
+                count+=1
+            pi = pi_next
+        if(count>=dim):
+            print "BREAK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            break
+
+    return pi
+
+'''___MAIN___'''
+l = 1e-1
+Q = np.array([[-1.0, 1.0, 0.0, 0.0],\
+              [1.0, -1.0-l, 0.0, l], \
+              [2*l, 0.0, -1.0-2*l, 1.0],\
+              [0.0, 0.0, 1.0, -1.0]])
+#print Q
+zero = [0.0, 0.0, 0.0, 0.0]#solution to Ax = b
+pi = [1.0, 0.0, 0.0, 0.0]#what we are attempting to solve for
+
+n = 46#number of iterations
+
+print "final result!!!"
+pi = gauss(Q, zero, pi, n, len(Q), l)
+print pi
+
