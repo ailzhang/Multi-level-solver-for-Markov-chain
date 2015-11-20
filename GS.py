0c2832b1017f778a3336ce281facb83f4594211c
saiyan86 <saiyan@Yonglis-MacBook.local>
2015-11-20 13:13:26 -0600

    wlfkjwelfjwklfw`

--- GS.py
+++ GS.py
@@ -2,9 +2,9 @@ import numpy as np
 from scipy.linalg import solve
 
 def gauss(Q, zero, pi, n, dim, lamda):
-    pi_next = [0.0, 0.0, 0.0, 0.0]
+    pi_next = [0.0, 0.0]
     count = 0
-    #print "Q=",Q,"zero=",zero,"pi=",pi,"dim=",dim,"lamda=",lamda
+    print "Q=",Q,"zero=",zero,"pi=",pi,"dim=",dim,"lamda=",lamda
     for k in range(1,n):
         for i in range(1,dim+1):
             count = 0
@@ -19,7 +19,7 @@ def gauss(Q, zero, pi, n, dim, lamda):
                 print "pi_next[",i-1,"]",pi_next[i-1]
                 '''
             for j in range(i+1,dim+1):
-                #print "i=",i,"j=",j
+                print "i=",i,"j=",j
                 pi_next[i-1] += (-1.0)*(pi[j-1]*Q[j-1][i-1])/Q[i-1][i-1]
                 '''
                 print "i=",i,"j=",j,"lower_half"
@@ -29,7 +29,7 @@ def gauss(Q, zero, pi, n, dim, lamda):
                 print "pi_next[",i-1,"]",pi_next[i-1]
             print str(k).zfill(4),
             print(pi_next)
-                '''
+            '''
             if(pi_next[i-1]-pi[i-1]<lamda):
                 count+=1
             pi = pi_next
@@ -37,7 +37,7 @@ def gauss(Q, zero, pi, n, dim, lamda):
             print "BREAK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
             break
 
-    return pi
+    return pi/np.sum(pi)
 
 '''___MAIN___'''
 l = 1e-1
@@ -45,13 +45,21 @@ Q = np.array([[-1.0, 1.0, 0.0, 0.0],\
               [1.0, -1.0-l, 0.0, l], \
               [2*l, 0.0, -1.0-2*l, 1.0],\
               [0.0, 0.0, 1.0, -1.0]])
+'''
+Q2 = np.array([[-2.0, 2.0, 0.0],\
+               [0.0, -1.0, 1.0], \
+               [1.5, 1.5, -3.0]])
+'''
+
+Q3 = np.array([[-1.0, 1.0],\
+              [1.0, -1.0]])
 #print Q
 zero = [0.0, 0.0, 0.0, 0.0]#solution to Ax = b
-pi = [1.0, 0.0, 0.0, 0.0]#what we are attempting to solve for
+pi = [1.0, 0.0]#what we are attempting to solve for
 
 n = 46#number of iterations
 
 print "final result!!!"
-pi = gauss(Q, zero, pi, n, len(Q), l)
+pi = gauss(Q3, zero, pi, n, 2, l)
 print pi
 
--- GaussSeidel.py
+++ GaussSeidel.py
@@ -3,21 +3,26 @@ from scipy.linalg import solve
 
 def gauss(A, b, x, n):
 
-    L = np.tril(A)
-    U = A - L
+    L = np.tril(A)#Lower triangle of A
+    U = A - L#Upper triangle of A
     for i in range(n):
-        x = np.dot(np.linalg.inv(L), b - np.dot(U, x))
-        print str(i).zfill(3),
+        x = np.dot(np.linalg.inv(L), b - np.dot(U, x))#dot of (inverse of L & dot of (upper triangle of A and x))
+        print str(i).zfill(4),
         print(x)
     return x
 
 '''___MAIN___'''
+l = 1e-1
+A = np.array([[-1.0, 1.0, 0.0, 0.0],\
+              [1.0, -1.0-l, 0.0, l], \
+              [2*l, 0.0, -1.0-2*l, 1.0],\
+              [0.0, 0.0, 1.0, -1.0]])
+print A
+b = [0.0, 0.0, 0.0, 0.0]#solution to Ax = b
+x = [0.25, 0.25, 0.25, 0.25]#what we are attempting to solve for
 
-A = np.array([[4.0, -2.0, 1.0], [1.0, -3.0, 2.0], [-1.0, 2.0, 6.0]])
-b = [1.0, 2.0, 3.0]
-x = [1, 1, 1]
-
-n = 20
+n = 5#number of iterations
 
 print gauss(A, b, x, n)
+print "final result"
 print solve(A, b)
