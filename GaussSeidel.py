1a6e166fdffd243be0e36404df7de0eef48cd94e
Ailing Zhang <zhangal1992@gmail.com>
2015-11-28 23:03:18 -0600

    MultiLevel algorithm done.

--- GaussSeidel.py
+++ GaussSeidel.py
@@ -3,9 +3,11 @@ from scipy.linalg import solve
 import pdb
 from BirthDeath import BirthDeath
 
-def gauss(Q, n, lamda):
+def GaussSeidel(Q, n, lamda, stopBylamda):
     pi = np.ones((n,1))
+    count = 0
     while True:
+        count = count + 1
         pi_next = np.zeros((n,1))
         for i in range(0,n):
             for j in range(0,i):
@@ -13,14 +15,16 @@ def gauss(Q, n, lamda):
             for j in range(i+1,n):
                 pi_next[i] += (-1.0)*(pi[j]*Q[j][i])/Q[i][i]
         pi_next = pi_next / np.sum(pi_next);
-        if(np.linalg.norm(pi_next - pi) < lamda):
+        if(stopBylamda == True and np.linalg.norm(pi_next - pi) < lamda):
+            return pi_next
+        if(stopBylamda == False and count == lamda):
             return pi_next
         pi = pi_next ;
 
 '''___MAIN___'''
 if __name__ == "__main__":
     lamda = 1e-7
-    n = 4
+    n = 6
     birth = 1
     death = 2
     Q = BirthDeath(n, birth, death)
@@ -30,6 +34,6 @@ if __name__ == "__main__":
 #              [0.0, 0.0, 1.0, -1.0]])
     print Q
     print "final result!!!"
-    pi = gauss(Q,n,lamda)
+    pi = GaussSeidel(Q,n,lamda, True)
     print pi
 
---
+++ MultiLevel.py
@@ -0,0 +1,72 @@
+#####################################################
+##      Created on Nov 28, 2015 by Ailing Zhang     ##
+#####################################################
+import numpy as np
+from BirthDeath import BirthDeath
+from GaussSeidel import GaussSeidel
+from numpy import linalg
+import pdb
+
+def MultiLevel(P, level):
+    n = P.shape[0];
+    if level == 0:
+#        return linalg.solve(P, np.zeros((n,1)))
+        return GaussSeidel(np.transpose(P), n, 1e-7, True)
+    else:
+        p_tilde = GaussSeidel(np.transpose(P), n, 3, False)
+        cluster = Partition(P);
+        P_next, p_tilde_next = Coarse(P, p_tilde, cluster);
+        p_bar_next = MultiLevel(P_next, level-1)
+        p_star_next = np.divide(p_bar_next, p_tilde_next)
+        p_star = I(p_star_next, n, cluster)
+        p_bar = C(p_tilde, p_star)
+        return p_bar / np.sum(p_bar)
+
+def Partition(P):
+    n = P.shape[0];
+    grid = 2
+    originalset = range(n)
+    cluster = [originalset[i:i+grid] for i in range(0, (n / grid - 1) * grid,grid)]
+    cluster.append(range((n/grid-1)*grid, n))
+    return cluster
+
+def Coarse(P, p_tilde, cluster):
+    n = len(cluster)
+    P_next = np.zeros((n,n))
+    p_tilde_next = np.zeros((n,1))
+    for i in range(n):
+        for j in cluster[i]:
+            p_tilde_next[i] += p_tilde[j]
+
+    for i in range(n):
+        for j in range(n):
+            sum = 0
+            for k in cluster[i]:
+                sum_i = 0
+                for l in cluster[j]:
+                    sum_i += P[l][k]
+                sum += sum_i * p_tilde[k]
+            P_next[j][i] = sum / p_tilde_next[i]
+    return (P_next, p_tilde_next)
+
+def I(p_star_next, n, cluster):
+    p_star = np.zeros((n,1))
+    for i in range(len(cluster)):
+        for j in cluster[i]:
+            p_star[j] = p_star_next[i]
+    return p_star
+
+def C(p_tilde, p_star):
+    p_bar = np.multiply(p_tilde, p_star)
+    return p_bar
+
+if __name__ == "__main__":
+    n = 6
+    birth = 1
+    death = 2
+    Q = BirthDeath(n, birth, death)
+    P = np.transpose(Q)
+    pdb.set_trace()
+    level = 1
+    pi = MultiLevel(P, level)
+    print pi
